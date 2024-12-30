d84426b7fb39cb3f88560426a20cc26a0d52b42a
cobalt <61329810+cobaltt7@users.noreply.github.com>
2024-12-29 23:26:18 -0600

    Add lint, beginning of user stats, pin dapi-types
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import cobaltConfigs, { declareConfig, globals } from "eslint-config-cobaltt7";
+
+export default declareConfig(
+	{ files: ["**/*.ts"] },
+	{ ignores: ["dist"] },
+	...cobaltConfigs,
+	{
+		languageOptions: {
+			ecmaVersion: 2025,
+			globals: globals.nodeBuiltin,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: path.dirname(fileURLToPath(import.meta.url)),
+			},
+		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/naming-convention": [
+				"error",
+				{
+					format: ["camelCase", "PascalCase", "UPPER_CASE"],
+					selector: ["import", "variable"],
+				},
+				{
+					format: ["camelCase"],
+					selector: ["function", "method", "parameterProperty"],
+				},
+				{
+					format: ["camelCase"],
+					leadingUnderscore: "allow",
+					selector: "parameter",
+				},
+				{
+					format: ["camelCase", "UPPER_CASE"],
+					selector: "classProperty",
+				},
+				{
+					format: ["PascalCase"],
+					selector: ["enumMember", "typeLike"],
+				},
+				{
+					custom: { match: false, regex: /^T[A-Z]/u.source },
+					format: ["PascalCase"],
+					leadingUnderscore: "allow",
+					selector: ["typeParameter"],
+				},
+			],
+		},
+	},
+	{ files: ["./common/typedefs/**"], rules: { "unicorn/filename-case": "off" } },
+);
--- modules/39.ts
+++ modules/39.ts
@@ -6,12 +6,11 @@ import { client, defineEvent } from "strife.js";
 import constants from "../common/constants.ts";
 import { ThirtyNine } from "../common/database.ts";
 
-const _39Regex = /\b(?:39|thirty[ -]?nine)\b/i;
-
-const last39s = new Collection<Snowflake, { hour: number; users: Set<Snowflake> }>();
+type Last39 = { hour: number; users: Set<Snowflake> };
+const last39s = new Collection<Snowflake, Last39>();
 
 defineEvent("messageCreate", async (message) => {
-	const has39 = message.guild && _39Regex.test(message.content);
+	const has39 = message.guild && /\b(?:39|thirty[ -]?nine)\b/i.test(message.content);
 	if (!has39) return;
 
 	const canReact = message.channel.isDMBased() || message.channel.permissionsFor(client.user);
@@ -33,34 +32,31 @@ defineEvent("messageCreate", async (message) => {
 
 	const last39 = await retriveLast39s({ hour, guild: message.guild.id });
 	if (minutes !== constants.triggerTime) {
-		if (last39?.hour === hour) {
+		if (last39.hour === hour)
 			if (last39.users.has(message.author.id)) {
 				if (canReact) await message.react(constants.emojis.miku);
 				return;
 			} else last39.users.add(message.author.id);
-		}
 
 		if (canReact) await message.react(constants.emojis.mikuSad);
 		await new ThirtyNine({
 			guild: message.guild.id,
 			user: message.author.id,
-			hour: hour,
+			hour,
 			missed: true,
 		}).save();
 		return;
 	}
 
-	if (last39?.hour === hour) {
+	if (last39.hour === hour) {
 		if (last39.users.has(message.author.id)) return;
 		last39.users.add(message.author.id);
-	} else {
-		last39s.set(message.guild.id, { hour, users: new Set([message.author.id]) });
-	}
+	} else last39s.set(message.guild.id, { hour, users: new Set([message.author.id]) });
 
 	await new ThirtyNine({
 		guild: message.guild.id,
 		user: message.author.id,
-		hour: hour,
+		hour,
 		seconds: seconds + milliseconds / 1000,
 		missed: false,
 	}).save();
@@ -68,17 +64,20 @@ defineEvent("messageCreate", async (message) => {
 		await message.react(constants.emojis[seconds === constants.triggerTime ? "miku39" : "_39"]);
 });
 
-async function retriveLast39s({ guild, hour }: { guild: Snowflake; hour: number }) {
+async function retriveLast39s({
+	guild,
+	hour,
+}: {
+	guild: Snowflake;
+	hour: number;
+}): Promise<Last39> {
 	const last39 = last39s.get(guild);
 	if (last39) return last39;
 
 	const [{ users } = { users: [] }] = await ThirtyNine.aggregate<{
 		_id: null;
 		users: Snowflake[];
-	}>([
-		{ $match: { guild: guild, hour: hour } },
-		{ $group: { _id: null, users: { $addToSet: "$user" } } },
-	]);
+	}>([{ $match: { guild, hour } }, { $group: { _id: null, users: { $addToSet: "$user" } } }]);
 
 	const data = { hour, users: new Set(users) };
 	last39s.set(guild, data);
